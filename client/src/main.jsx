1b1118c4c28f37db18bb1cf5195cc17f5662340a
RishabhAgni2 <rishabhagni68107@gmail.com>
2026-06-04 00:17:36 +0530

    chore: setup entry files with Redux Provider and Toaster

---
+++ client/src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import App from './App.jsx';
+import { store } from './store/index.js';
+import './index.css';
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: 1, staleTime: 30000 } },
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
+  </React.StrictMode>
+);
